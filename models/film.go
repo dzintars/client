7e2fbb3ab846c1a702535248f5387b59f2336f1f
oswee <dzintars.klavins@gmail.com>
2018-07-23 14:13:15 +0300

    Code refactoring (working)

--- models/film.go
+++ models/film.go
@@ -7,10 +7,16 @@ import (
 	"github.com/oswee/proto"
 )
 
-func ListFilms(c proto.StarfriendsClient, req proto.ListFilmsRequest) {
-
+// ListFilms retrieve list of all films
+func ListFilms() (*proto.ListFilmsResponse, error) {
+	cc := gLoc()
+	defer cc.Close()
+	// We can now create stubs that wrap cc:
+	c := proto.NewStarfriendsClient(cc)
+	req := &proto.ListFilmsRequest{}
 	res, err := c.ListFilms(context.Background(), req)
 	if err != nil {
 		log.Fatalf("Error while calling ListFilms RPC: %v", err)
 	}
+	return res, nil
 }
--- routes/index.go
+++ routes/index.go
@@ -1,15 +1,12 @@
 package routes
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/oswee/client/models"
 	"github.com/oswee/client/utils"
-	"github.com/oswee/proto"
-	"google.golang.org/grpc"
 )
 
 func indexHandler(r *mux.Router) {
@@ -18,28 +15,10 @@ func indexHandler(r *mux.Router) {
 
 func indexGetHandler(w http.ResponseWriter, r *http.Request) {
 
-	cc, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Could not connect: %v", err)
-	}
-	defer cc.Close()
-	// We can now create stubs that wrap conn:
-	c := proto.NewStarfriendsClient(cc)
-
-	data, err := doUnary(c)
+	data, err := models.ListFilms()
 	if err != nil {
 		log.Fatalf("Error while calling ListFilms RPC: %v", err)
 	}
-	fmt.Println(data)
 
 	utils.ExecuteTemplate(w, "index.html", data)
-}
-
-func doUnary(c proto.StarfriendsClient) (*proto.ListFilmsResponse, error) {
-	req := &proto.ListFilmsRequest{}
-	res, err := c.ListFilms(context.Background(), req)
-	if err != nil {
-		log.Fatalf("Error while calling ListFilms RPC: %v", err)
-	}
-	return res, nil
 }
