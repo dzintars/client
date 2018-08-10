310156bbe60831f41143f162132a21117e741366
oswee <dzintars.klavins@gmail.com>
2018-08-10 11:04:18 +0300

    GetApplication gRPC (working)

--- routes/index.go
+++ routes/index.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/oswee/client/models"
 	"github.com/oswee/client/utils"
-	"github.com/oswee/proto"
 	app "github.com/oswee/proto/application/go"
 )
 
@@ -17,28 +16,21 @@ func indexHandler(r *mux.Router) {
 
 func indexGetHandler(w http.ResponseWriter, r *http.Request) {
 
-	films, err := models.ListFilms()
-	if err != nil {
-		log.Fatalf("Error while calling ListFilms RPC: %v", err)
-	}
-
-	apps, err := models.ListApplications(100)
+	applications, err := models.ListApplications(100)
 	if err != nil {
 		log.Fatalf("Error while calling ListApplications model: %v", err)
 	}
 
-	abc, err := models.GetApplication(10)
+	application, err := models.GetApplication(10)
 	if err != nil {
 		log.Fatalf("Error while calling GetApplication model: %v", err)
 	}
 
 	utils.ExecuteTemplate(w, "index.html", struct {
-		Films []*proto.Film
-		Apps  []*app.Application
-		Appx  *app.Application
+		Applications []*app.Application
+		Application  *app.Application
 	}{
-		Films: films.Films,
-		Apps:  apps.Applications,
-		Appx:  abc.Application,
+		Applications: applications.Applications,
+		Application:  application.Application,
 	})
 }
