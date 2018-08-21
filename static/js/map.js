9ee5360963473e5b1c0da07d1460113a21f31692
oswee <dzintars.klavins@gmail.com>
2018-08-21 21:33:09 +0300

    SVG marker improvements

--- static/js/map.js
+++ static/js/map.js
@@ -23,13 +23,25 @@ if (leafletMap) {
     }).addTo(mymap);
 
     for (var i = 0; i < markers.length; i++) {
-        svgicon = `<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 365 560' enable-background='new 0 0 365 560' xml:space='preserve'>
-            <style type='text/css'>.st0{fill:#00AEEF;} .st1{font-family:'Montserrat-Light';} .st2{font-size:256px;}</style>
-            <g>
-                <path fill='#006AFF' d='M182.9,551.7c0,0.1,0.2,0.3,0.2,0.3S358.3,283,358.3,194.6c0-130.1-88.8-186.7-175.4-186.9 C96.3,7.9,7.5,64.5,7.5,194.6c0,88.4,175.3,357.4,175.3,357.4S182.9,551.7,182.9,551.7z M122.2,187.2c0-33.6,27.2-60.8,60.8-60.8 c33.6,0,60.8,27.2,60.8,60.8S216.5,248,182.9,248C149.4,248,122.2,220.8,122.2,187.2z'/>
-                <text transform='matrix(1 0 0 1 169 207)' class='st1 st2'>` + markers[i][1] + `</text>
-            </g>
-        </svg>`;
+        svgicon = `<svg id="Marker" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+        <defs>
+          <style>
+            .cls-1 {
+              fill: #006aff;
+            }
+      
+            .cls-2 {
+              font-size: 28px;
+              fill: #fff;
+              font-family: Montserrat-Regular, Montserrat;
+              text-align: center;
+            }
+          </style>
+        </defs>
+        <title>marker-01</title>
+        <path class="cls-1" d="M56.9679,24.9679A24.9679,24.9679,0,1,0,12.38928,40.42236h-.00067l18.4848,22.96552a1.38676,1.38676,0,0,0,2.22162.10583l17.86951-22.285H50.963A24.86853,24.86853,0,0,0,56.9679,24.9679Z"/>
+        <text class="cls-2" transform="translate(18 34)">` + markers[i][1] + `</text>
+      </svg>`
     
         url = encodeURI("data:image/svg+xml," + svgicon).replace('#','%23');
     
